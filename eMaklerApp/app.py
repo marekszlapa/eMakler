ad492d7fc9bd4fbb92ebf96504af57a4132f39bb
marekszlapa <marekszlapa@o2.pl>
2022-06-12 16:57:27 +0200

    Podstawowy wygląd zakupu akcji

--- eMaklerApp/app.py
+++ eMaklerApp/app.py
@@ -3,5 +3,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/stock')
+def stock_page():
+    stocks = [
+        {'id': 1, 'name': 'Akcja1', 'price': 100},
+        {'id': 2, 'name': 'Akcja2', 'price': 200},
+        {'id': 3, 'name': 'Akcja3', 'price': 300}
+    ]
+    return render_template('stock.html', stocks=stocks)

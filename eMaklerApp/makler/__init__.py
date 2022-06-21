847bc01d825f8e7db047de41e5f2f4b6a4c9acc6
marekszlapa <marekszlapa@o2.pl>
2022-06-21 15:18:58 +0200

    wylogowywanie/komunikaty

--- eMaklerApp/makler/__init__.py
+++ eMaklerApp/makler/__init__.py
@@ -10,6 +10,9 @@ app.config['SECRET_KEY'] = '9b6cd67db047e659c56ac078'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+login_manager.login_message = "Aby uzyskać dostęp do tej strony musisz się zalogować"
 finnhub_client = finnhub.Client(api_key="caitgjqad3i2a9kcigrg")
 
 from makler import routes

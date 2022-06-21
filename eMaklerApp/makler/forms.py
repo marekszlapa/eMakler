95646dfe3352199dfefc75be72a5e6e511bfeba5
Dawid <David1357@wp.pl>
2022-06-21 02:03:57 +0200

    Formularz rejestracji - walidacja

--- eMaklerApp/makler/forms.py
+++ eMaklerApp/makler/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
-    submit = SubmitField(label='Create Account')
+    username = StringField(label='Nazwa użytkownika:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Adres e-mail:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Hasło:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Potwierdź hasło:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Stwórz konto')
--- eMaklerApp/makler/routes.py
+++ eMaklerApp/makler/routes.py
@@ -1,8 +1,9 @@
 from makler import app
-from flask import render_template
-from makler.models import Akcje
+from flask import render_template, redirect, url_for
+from makler.models import Akcje, User
 from makler import finnhub_client
 from makler.forms import RegisterForm
+from makler import db
 
 @app.route('/')
 @app.route('/home')
@@ -27,4 +28,14 @@ def stock_page():
 @app.route('/register')
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'Wystąpił błąd przy tworzeniu konta: {err_msg}')
     return render_template('register.html', form=form)
